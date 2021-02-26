c0bb0b073b178c9edf209f1d9cd14a7be382f591
Sanad <s_wj_88@hotmail.com>
2021-02-26 05:28:39 +0200

    Add input, select and a button to BokksForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const BooksForm = () => (
-  <div>BooksForm Component</div>
-);
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const BooksForm = () => {
+  const handleClick = () => {
+
+  };
+  return (
+    <div>
+      <input />
+      <select>
+        {categories.map(category => (
+          <option key={category}>
+            {' '}
+            {category}
+          </option>
+        ))}
+      </select>
+      <button type="button" value="Submit" onClick={handleClick}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
 export default BooksForm;
