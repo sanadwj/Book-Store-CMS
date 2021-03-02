e7998030d90b8486a8495594f6b0653cadd773e2
Sanad <s_wj_88@hotmail.com>
2021-03-02 04:07:36 +0200

    Add handleClick to remove book from booksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
 const BooksList = () => {
   const books = useSelector(state => state.books);
+  const dispatch = useDispatch();
+
+  const handleClick = e => {
+    e.preventDefault();
+    const book = books.map(book => book.id);
+    dispatch(removeBook(book));
+  };
   console.log(books);
   return (
     <table>
@@ -15,7 +23,7 @@ const BooksList = () => {
         </tr>
       </thead>
       <tbody>
-        {books.map(book => <Book key={book.id} book={book} />)}
+        {books.map(book => <Book key={book.id} book={book} remove={handleClick} />)}
       </tbody>
     </table>
   );
